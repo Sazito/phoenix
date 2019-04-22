2c876f51eae8d8e76ae11730b9c03352ac6ce751
amirhossein693 <amirhossein693@gmail.com>
2019-04-22 14:34:29 +0430

    add sample test for peoject

---
+++ src.example/routers/__test__/routes.test.js
@@ -0,0 +1,22 @@
+import routes from '../routes';
+
+const testRoutes = (routes) => {
+  test('routes is not null or undefined', () => {
+    expect(routes).toEqual(expect.anything());
+  });
+  describe.each(routes)(
+    'route index %#',
+    testEachRoute
+  );
+};
+
+const testEachRoute = record => {
+  if (record.routes) {
+    testRoutes(record.routes);
+  }
+  test('route have path?', () => {
+    expect(record).toHaveProperty('path');
+  });
+};
+
+testRoutes(routes);
