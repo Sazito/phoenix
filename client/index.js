cc82f8285b569aabca498f80f695159303a4cd3e
Amirhossein <amirhossein693@gmail.com>
2019-05-20 14:37:18 +0430

    user and api creation in dev mode

--- client/index.js
+++ client/index.js
@@ -8,13 +8,29 @@ import createStore from "../store";
 import { createUser } from "../modules/membership";
 import rootSaga from "../code/redux/root_saga";
 import isProduction from "../modules/utils/is_production";
+import { checkUser } from "../code/modules/membership";
+import { env } from "../code/configs";
+import { createAPI } from "../modules/api_wrapper";
+import Cookies from "js-cookie";
 
 const root = document.querySelector("#root");
 
 // get initial state from server and use it for creating redux store
 const store = createStore({ initState: window.__SERVER_STATES__ || {} });
 
-const theUser = createUser({ initContext: window.__CONTEXT__ || null });
+const token = Cookies.get(env.APP_TOKEN);
+createAPI({ token });
+
+let theUser;
+if (isProduction) {
+  // in production we have SSR
+  theUser = createUser({ initContext: window.__CONTEXT__ || null });
+} else {
+  theUser = createUser({
+    token,
+    checkUser
+  });
+}
 
 // we need to start sagas outside the Redux middleware environment
 // because of running necessary sagas for pre-fetching data for server side rendering on server app
