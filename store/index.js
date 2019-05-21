c48432d2fc62f48799a27fff8db4440f83425e8a
Amirhossein <amirhossein693@gmail.com>
2019-05-21 19:29:13 +0430

    disable redux devtools from production mode

--- store/index.js
+++ store/index.js
@@ -1,9 +1,18 @@
 import createSagaMiddleware, { END } from "redux-saga";
-import { applyMiddleware, createStore as createReduxStore, compose } from "redux";
+import {
+  applyMiddleware,
+  createStore as createReduxStore,
+  compose
+} from "redux";
 import rootReducer from "../code/redux/root_reducers";
+import isProduction from "../modules/utils/is_production";
 
 const createStore = ({ initState = {} } = {}) => {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    (!isProduction &&
+      typeof window === "object" &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
   const sagaMiddleware = createSagaMiddleware();
   const store = createReduxStore(
     rootReducer,
