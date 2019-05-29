56bd2351ac5eab3e6d91f2eb173eb2d0faaea73e
amirhossein693 <amirhossein693@gmail.com>
2019-05-29 14:16:16 +0000

    Feature/add acl check

---
+++ modules/membership/acl_check.js
@@ -0,0 +1,28 @@
+import { getRole, getRoleConfigs } from '../../code/configs';
+
+const hasPermission = (permissions, roleConfigs) => {
+  return permissions.find(permission => {
+    return (roleConfigs.permissions || []).indexOf(permission) >= 0
+  })
+};
+
+const aclCheck = ({permissions, user}) => {
+  const roleName = getRole({user});
+  const roleConfigs = getRoleConfigs(roleName);
+  let hasAccess = false;
+  if (!permissions || permissions.length === 0) {
+    hasAccess = true;
+  } else {
+    if (permissions && user && 'permissions' in roleConfigs && !!hasPermission(permissions, roleConfigs)) {
+      hasAccess = true;
+    } else {
+      hasAccess = false;
+    }
+  }
+  return {
+    hasAccess,
+    roleConfigs
+  }
+};
+
+export default aclCheck;
---
+++ modules/router_helper/with_acl.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import aclCheck from '../membership/acl_check';
+import UserContext from "../user_context";
+
+const WithACL = ({ children, permissions, onReject = "defaultRoute" }) => {
+  return (
+    <UserContext.Consumer>
+      {({ user }) => {
+        const acl = aclCheck({permissions, user});
+        if (acl.hasAccess) return children;
+        else return (
+          <Redirect
+            to={{
+              pathname: acl.roleConfigs[onReject] || acl.roleConfigs["defaultRoute"]
+            }}
+          />
+        )
+      }}
+    </UserContext.Consumer>
+    );
+};
+
+export default WithACL;
