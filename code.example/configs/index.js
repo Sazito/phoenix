85b93c4bc94d00bccdd9ab08dac41057780013a3
amirhossein693 <amirhossein693@gmail.com>
2019-06-02 17:04:05 +0000

    configs bugfix

--- code.example/configs/index.js
+++ code.example/configs/index.js
@@ -2,4 +2,6 @@ import env from "./env";
 import endpoints from "../../modules/endpoints_helper";
 
 export * from "../../modules/endpoints_helper";
+export * from "./acl/permissions";
+export * from "./acl/roles";
 export { env, endpoints };
