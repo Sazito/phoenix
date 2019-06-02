ef78a3589ad1b6577b342f812aea46307b8e40e1
amirhossein693 <amirhossein693@gmail.com>
2019-06-02 16:29:19 +0000

    Bugfix/user transformer

--- code.example/pages/login_page/index.js
+++ code.example/pages/login_page/index.js
@@ -1,47 +1,12 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import { pLogin } from "./login_page.scss";
-import { getCurrentUser } from "../../redux/users/actions";
-import Loading from "../../components/loading";
 
-const mapStateToProps = state => {
-  return {
-    isFetched: state.users.isFetched,
-    isLoading: state.users.isLoading,
-    user: state.users.data
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onGetUser: () => {
-      return dispatch(getCurrentUser());
-    }
-  };
-};
-
-const LoginPage = ({ onGetUser, isFetched, isLoading, user }) => {
-  useEffect(() => {
-    !isFetched && onGetUser();
-  }, []);
-
+const LoginPage = () => {
   return (
-    <>
-      {isLoading && <Loading />}
-      <div className={pLogin}>
-        {!isLoading && user && (
-          <div>
-            <div>Welcome {user.username}</div>
-          </div>
-        )}
-      </div>
-    </>
+    <div className={pLogin}>
+      Coming Soon ;)
+    </div>
   );
 };
 
-const LoginPageWrapper = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LoginPage);
-
-export default LoginPageWrapper;
+export default LoginPage;
