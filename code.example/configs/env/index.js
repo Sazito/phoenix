b317f39dd43a3d7c88a196422f37c076b2ae8ff0
Amirhossein <amirhossein693@gmail.com>
2019-12-30 12:58:47 +0330

    bugfix/token_and_proxy

--- code.example/configs/env/index.js
+++ code.example/configs/env/index.js
@@ -1,6 +1,7 @@
 const env = {
   APP_NAME: process.env.APP_NAME,
-  APP_API_BASE: process.env.APP_API_BASE
+  APP_API_BASE: process.env.APP_API_BASE,
+  APP_TOKEN: process.env.APP_TOKEN
 };
 
 export default env;
