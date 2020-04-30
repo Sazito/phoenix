eabbc5332a213373f8d43e2dc2199fe9d4bd47af
A.Kadir Mutlu <kingemkurdam@gmail.com>
2020-05-01 01:22:32 +0300

    Fix Local File Inclusion vulnerability.
    Fix Local File Inclusion vulnerability
    Attacker can acces local file via assets router.
    Sample Attack:  /assets/../../../../../../../../etc/passwd

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -2,8 +2,24 @@ import path from "path";
 import fs from "fs";
 const BASEPATH = process.env.BASEPATH;
 
+// From: https://github.com/pillarjs/send/blob/master/index.js#L63
+var UP_PATH_REGEXP = /(?:^|[\\/])\.\.(?:[\\/]|$)/;
+
 const assets = (req, res) => {
   let filePath = req.originalUrl;
+
+  try {
+    filePath = decodeURIComponent(filePath);
+  } catch (err) {
+    return res.status(400).send(new Error("invalid url"));
+  }
+  if (~filePath.indexOf("\0")) {
+    return res.status(401).send(new Error("null byte attack dedected!!"));
+  }
+  if (UP_PATH_REGEXP.test(filePath)) {
+    return res.status(403).send(new Error("LFI attack dedected!!!"));
+  }
+
   if (BASEPATH) {
     filePath = req.originalUrl.replace(`/${BASEPATH}`, ``);
   }
