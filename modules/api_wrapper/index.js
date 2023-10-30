fadf0b83420f010f608869eca0fd9bb3bd4e509a
MohammadReza Mahmoudi <rezamahmudy1@gmail.com>
2023-10-30 20:27:59 +0330

    change apiWrapper

--- modules/api_wrapper/index.js
+++ modules/api_wrapper/index.js
@@ -7,8 +7,7 @@ const api = {};
 const APICreator = ({ method, token }) => {
   return (endpoint, body, base) => {
     const isAbsolute = endpoint.toLowerCase().startsWith("http");
-    console.log({base, full: `${base || env.APP_API_BASE}${endpoint}`})
-    const url = isAbsolute ? endpoint : `${base || env.APP_API_BASE}${endpoint}`;
+    const url = isAbsolute ? endpoint : `${env.APP_API_BASE}${endpoint}`;
     const options = {
       method: method.toUpperCase(),
       body: JSON.stringify(body),
